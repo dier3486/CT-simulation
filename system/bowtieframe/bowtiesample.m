4e699cc7f1b2d69997e2d594b95b281bf89db0c0
Dier <diorland_xh@163.com>
2019-11-18 15:26:40 +0800

    fix a bug in cfg path

--- system/bowtieframe/bowtiesample.m
+++ system/bowtieframe/bowtiesample.m
@@ -1,7 +1,21 @@
 function bowtie_corr = bowtiesample()
 % bowtie corr sample
+% WARN: DO NOT COPY THIS CURVE TO REAL PRODUCT
 
-bowtie.ID = [0 0 0 0];
-bowtie.box = [140, 20, 50];
-bowtie.focaltobottom = 150;
+% smample curves
+minthick = 2;
+maxthick = 50;
+length = 180;
+curve1 = bowtiecurvesample(minthick, maxthick, length, 12.5, 0.18);
+curve2 = bowtiecurvesample(minthick, maxthick, length, 16.5, 0.14);
+
+% corr struct
+bowtie_corr.ID = uint8([0 0 0 0]);
+bowtie_corr.box = single([140, 20, 50]);
+bowtie_corr.focaltobottom = single(150);
+bowtie_corr.Ncurve = uint32(2);
+bowtie_corr.Nsample = uint32(2);
+bowtie_corr.main = single([curve1 curve2(:,2)]);
+
+end
 
