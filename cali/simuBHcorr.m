f5b9a46d4775b2f440e25f2f9a38003593e01501
Dier <diorland_xh@163.com>
2019-12-04 18:02:33 +0800

    daily

--- cali/simuBHcorr.m
+++ cali/simuBHcorr.m
@@ -33,7 +33,11 @@ Dfilter = reshape(-log(exp(-Dfmu)*(samplekeV(:).*response))./mu_weff, Npixel, Ns
 Deff = Dfilter-Dempty;
 Dfmu = reshape(Dfmu, Npixel, Nslice, Nsample);
 
-ilsice = 1;
+m = 4; n = 4;
+a = 400; b = 100;
+bhpoly = zeros(Npixel*Nslice, m);
+
+islice = 1;
 % for islice = 1:Nslice
     [Deff_ii, sort_eff] = sort(Deff(:, islice));
     Dfmu_ii = squeeze(Dfmu(sort_eff, islice, :));
@@ -50,13 +54,35 @@ ilsice = 1;
     Pii = exp(-repmat(Dfmu_ii, Ndw, 1)+repelem(Dwmu, Nrep, 1))*(samplekeV(:).*response);
     Dii = reshape(-log(Pii)./mu_weff, Nrep, Ndw) - Dfilter_ii;
     Dtarget = Dwater./Dii;
-    m = 4; n = 3;
+    
     x0 = zeros(m*n, 1);
     x0(1) = 1;
-    a = 400; b = 100;
-    tic;
+    
     x = lsqnonlin(@(x) polyval2dm(reshape(x, m, n), Dii(:)./a, Deff_ii(:)./b) - Dtarget(:), x0);
-    toc
+    x = reshape(x, m, n);
+    % check error
+    err_ii = reshape(polyval2dm(x, Dii(:)./a, Deff_ii(:)./b) - Dtarget(:), Nrep, Ndw);
+    
+    Deff_ply = ones(Npixel, n);
+    for ii = 2:n
+        Deff_ply(:, ii) = (Deff(:, islice)./b).^(ii-1);
+    end
+    index_ii = (1:Npixel) + (islice-1)*Npixel;
+    bhpoly(index_ii, m) = Deff_ply*x(1, :)';
+    for ii = 1:m-1
+        bhpoly(index_ii, m-ii) = Deff_ply*x(ii+1, :)'./a^ii;
+    end
+    bhpoly(:, 1:m-1) = bhpoly(:, 1:m-1)./bhpoly(:, 2:m);
+    
+    % check
+    Dchk = 180;
+    Pchk = exp(-squeeze(Dfmu(:,islice,:))-Dchk.*mu_water(:)')*(samplekeV(:).*response);
+    Dchk_bh = -log(Pchk)./mu_weff - Dfilter(:, islice);
+    Dchk_corr = ones(size(Dchk_bh));
+    for ii=1:m
+        Dchk_corr = Dchk_corr.*Dchk_bh.*bhpoly(index_ii, ii) + 1.0;
+    end
+    Dchk_corr = Dchk_corr - 1.0;
     
     
 % end
