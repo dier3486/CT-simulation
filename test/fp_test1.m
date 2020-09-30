34789ec9e0267b858edad654d3a0e8dae4162610
dier3486 <diorland_xh@163.com>
2020-09-30 18:59:47 +0800

    daily

--- test/fp_test1.m
+++ test/fp_test1.m
@@ -8,7 +8,7 @@ A = [0 -200 0];
 B = [linspace(-200, 200, Np)' ones(Np,1).*200 zeros(Np,1)];
 h = 1;
 
-theta = 0.1;
+viewangle = 0.1;
 
 % go
 Cimage_ext = [Cimage(:); 0];
@@ -18,14 +18,14 @@ Lxy = sqrt((B(:,1)-A(:,1)).^2 + (B(:,2)-A(:,2)).^2);
 % Lo is the length from ISO to A (on xy plane)
 Lo = sqrt(A(:,1).^2 + A(:,2).^2);
 
-% % d is the distance from ISO to AB
-% d = (A(:,2).*B(:,1)-A(:,1).*B(:,2))./Lxy;
-% Lmid = sqrt(A(:,1).^2+A(:,2).^2-d.^2);
+% d is the distance from ISO to AB
+d = (A(:,2).*B(:,1)-A(:,1).*B(:,2))./Lxy;
+Lmid = sqrt(A(:,1).^2+A(:,2).^2-d.^2);
         
 % Xgrid = -(Nx-1)/2:(Nx-1)/2;
 % Ygrid = -(Ny-1)/2:(Ny-1)/2; 
 
-Rt = 100;
+Rt = 160;
 t = 0.9;
 tgrid = [fliplr(0:-t:-Rt) (t:t:Rt)]' + Lo;
 Nt = length(tgrid);
@@ -37,8 +37,8 @@ Rp2 = sum(Psample.^2, 2);
 Sp = Rp2<Rt^2;
 P0 = Psample(Sp, :);
 
-Mrot = [cos(theta)  sin(theta)    0;
-       -sin(theta)  cos(theta)    0;
+Mrot = [cos(viewangle)  sin(viewangle)    0;
+       -sin(viewangle)  cos(viewangle)    0;
         0               0         1];
 Pv = P0*Mrot - 0.5;
 
@@ -56,4 +56,16 @@ Dintp = sum(Cimage_ext(index_intp).*alpha_intp, 2);
 Dsample = zeros(Np, Nt);
 Dsample(Sp) = Dintp;
 
+Dv = sum(Dsample, 2).*t;
 
+
+% 0
+Xgrid = -Nx/2:Nx/2;
+Ygrid = -Ny/2:Ny/2;
+% angles
+Arot = A*Mrot;
+Brot = B*Mrot;
+theta = atan2(Brot(:,2)-Arot(:,2), Brot(:,1)-Arot(:,1));
+% call 2D projection function
+[dt, Vindex] = linesinimage2D(theta, d, Lxy, Lmid, Xgrid, Ygrid);
+D0 = sum(dt.*Cimage_ext(Vindex), 2);
