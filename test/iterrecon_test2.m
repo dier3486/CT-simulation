99654e7aea18cb01939719d1ed087dc9f6e27e9b
dier3486 <dier3486@gamil.com>
2021-09-10 19:12:27 +0800

    daily

--- test/iterrecon_test2.m
+++ test/iterrecon_test2.m
@@ -1,19 +1,30 @@
 % after itertestdata_script.m
 
+% part a
+pbm_a = pbm;
+pbm_a.Nview = 500;
+pbm_a.viewangle = pbm.viewangle(1:500);
+P1a = P1(:,1:500);
+
+% part b
+Npart = 900;
+pbm_b = pbm;
+pbm_b.Nview = Npart;
+pbm_b.viewangle = pbm.viewangle(1:Npart);
+P1b = P1(:,1:Npart);
 
 % filters
 Klr = gpuArray(filterdesign('ram-lak', pbm.Npixel, pbm.delta_d, 2.0));
 
 % main filter
-alpha_lr = 0.0;
+alpha_lr = 0;
 alpha_filt = 1.5;
 Kfilt = gpuArray(filterdesign('hann', pbm.Npixel, pbm.delta_d, alpha_filt));
 Kfilt = Kfilt.*(1-alpha_lr) + Klr.*alpha_lr;
 
 % FBP
-img0a = filterbackproj2D(P0, pbm, Kfilt);
-img1a = filterbackproj2D(P1, pbm, Kfilt);
-% img2a = filterbackproj2D(P2, pbm, Kfilt);
+img1a = filterbackproj2D(P1a, pbm_a, Kfilt);
+img1b = filterbackproj2D(P1b, pbm_b, Kfilt);
 
 % noise filter
 alpha_filt = 1.5;
@@ -31,9 +42,10 @@ Kf1 = Kfilt_BV.*pi;
 L = splaplace2D(double(pbm.imagesize));
 
 % D weight
-sigma_ERF = inf;
-sigma1 = 1./sqrt(exp(-P1.*1e-3.*mu_e).*Ndose)./mu_e.*1e3./sqrt(pbm.Nview*pbm.imagesize);
-D = erf(sigma_ERF./sigma1);
+sigma_ERF = 0.5;
+sigma1 = 1./sqrt(exp(-P1b.*1e-3.*mu_e).*Ndose)./mu_e.*1e3./sqrt(pbm.Nview*pbm.imagesize);
+D = gpuArray(ones(size(P1), classGPU(P1b)));
+D(:, 1:Npart) = erf(sigma_ERF./sigma1);
 D(D<0.05) = 0.05;
 
 % paramters
@@ -48,9 +60,9 @@ Niter = 20;
 alpha = 0.2;
 
 % vector ini
-b1 = img1a;
+b1 = img1b;
 u = zeros(pbm.imagesize, pbm.imagesize, Niter, 'single');
-u(:,:,1) = b1;
+u(:,:,1) = img1a;
 Gu = u;
 rerr = nan(Niter-1, 1);
 
@@ -78,14 +90,19 @@ for ii = 1:Niter-1
     end
     
     % style#1 project u;
-    % v1 = parallelprojinimageGPU(pbm, u(:,:,ii)+1i.*R_BV.*mu_F);
+    % v1 = parallelprojinimageGPU(pbm, u(:,:,ii));
     % style#2 project Gu
-%     v1 = parallelprojinimageGPU(pbm, Gu(:,:,ii)+1i.*R_BV.*mu_F);
+%     v1 = parallelprojinimageGPU(pbm, Gu(:,:,ii));
     % style#3 conjugate
-    v1 = parallelprojinimageGPU(pbm, (Gu(:,:,ii)+u(:,:,ii))./2+1i.*R_BV.*mu_F);
+    v1 = parallelprojinimageGPU(pbm_b, (Gu(:,:,ii)+u(:,:,ii))./2);
+    % v2
+    v2 = parallelprojinimageGPU(pbm, R_BV.*mu_F)./D;
+    % FBP
+    r1 = filterbackproj2D(v1, pbm_b, Kfilt);
+    r2 = filterbackproj2D(v2, pbm, Kf2);
     
-    v = real(v1) + fconv(imag(v1), Kf1)./D;
-    r = b1 - filterbackproj2D(v, pbm, Kfilt);
+%     v = real(v1) + fconv(imag(v1), Kf1)./D;
+    r = b1 - r1 - r2;
     u(:,:,ii+1) = u(:,:,ii) + gather(r.*alpha);
     
     rerr(ii) = gather(sqrt(sum(r(:).^2)).*(1/pbm.imagesize^2));
