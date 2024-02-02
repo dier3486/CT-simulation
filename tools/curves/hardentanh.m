2d8eb23186ad3f4fe3c4fd178a09d315e4222c74
dier3486 <dier3486@gmail.com>
2024-02-02 17:57:29 +0800

    update

--- tools/curves/hardentanh.m
+++ tools/curves/hardentanh.m
@@ -12,9 +12,13 @@ if abs(1-sigma) < eps
     % it is tanh
     y = (1-exp(-x.*2./c))./(1+exp(-x.*2./c)).*c;
 else
-    b = (1+sigma)/(1-sigma)*log(sigma)/c;
+    b = (1+sigma)/(1-sigma)*log(sigma)./c;
     c = -c./log(sigma); 
     y = log((1 + sigma.*exp(x.*b))./(sigma + exp(x.*b))).*c;
+end
+
+if any(isnan(y(:)))
+    warning('Please use the robust version hardentanh2 to replace the hardentanh!');
 end
 
 end
